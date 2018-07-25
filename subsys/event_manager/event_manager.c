8d0fad05b53d58b32e4e85198842c3dc3af007bb
Marek Pieta <Marek.Pieta@nordicsemi.no>
2018-07-25 08:43:52 +0200

    Dynamic memory allocation for array of events' IDs in event manager, changed IFDEF to IS_ENABLED

--- subsys/event_manager/event_manager.c
+++ subsys/event_manager/event_manager.c
@@ -15,9 +15,7 @@ K_WORK_DEFINE(event_processor, event_processor_fn);
 
 static sys_dlist_t eventq = SYS_DLIST_STATIC_INIT(&eventq);
 
-#ifdef CONFIG_DESKTOP_EVENT_MANAGER_PROFILER_ENABLED
-static uint16_t profiler_event_ids[CONFIG_MAX_NUMBER_OF_CUSTOM_EVENTS];
-#endif
+static u16_t *profiler_event_ids;
 
 static void event_processor_fn(struct k_work *work)
 {
@@ -50,15 +48,13 @@ if (sys_dlist_is_empty(&eventq)) {
 
 		const struct event_type *et = eh->type_id;
 
-#ifdef CONFIG_DESKTOP_EVENT_MANAGER_TRACE_EVENT_EXECUTION
-		{
+		if (IS_ENABLED(CONFIG_DESKTOP_EVENT_MANAGER_TRACE_EVENT_EXECUTION)) {
 			struct log_event_buf buf;
 			event_log_start(&buf);
 			event_log_add_mem_address(eh, &buf);
 			/* Event execution start in event manager has next id after all the events */
 			event_log_send(profiler_event_ids[__stop_event_types - __start_event_types +1], &buf); 
 		}
-#endif
 		if (IS_ENABLED(CONFIG_DESKTOP_EVENT_MANAGER_SHOW_EVENTS)) {
 			printk("e: %s ", et->name);
 			if (et->print_event) {
@@ -95,15 +91,13 @@ if (sys_dlist_is_empty(&eventq)) {
 				}
 			}
 		}
-#ifdef DESKTOP_EVENT_MANAGER_TRACE_EVENT_EXECUTION
-		{
+		if (IS_ENABLED(DESKTOP_EVENT_MANAGER_TRACE_EVENT_EXECUTION)) {
 			struct log_event_buf buf;
 			event_log_start(&buf);
 			event_log_add_mem_address(eh, &buf);
 			/* Event execution end in event manager has next id after all the events adn event execution start */
 			event_log_send(profiler_event_ids[__stop_event_types - __start_event_types +2], &buf);
-		}
-#endif		
+		}		
 		k_free(eh);
 	}
 
@@ -121,7 +115,7 @@ void _event_submit(struct event_header *eh)
 
 	irq_unlock(flags);
 
-#ifdef CONFIG_DESKTOP_EVENT_MANAGER_PROFILER_ENABLED	
+	if (IS_ENABLED(CONFIG_DESKTOP_EVENT_MANAGER_PROFILER_ENABLED)) {	
 		const struct event_type *et = eh->type_id;
 		if(et->log_args) {		
 			struct log_event_buf buf;
@@ -130,7 +124,7 @@ void _event_submit(struct event_header *eh)
 			et->log_args(eh, &buf);
 			event_log_send(profiler_event_ids[et - __start_event_types], &buf);
 		}			
-#endif
+	}
 	k_work_submit(&event_processor);
 }
 
@@ -140,7 +134,6 @@ static void event_manager_show_listeners(void)
 	for (const struct event_listener *el = __start_event_listeners;
 	     el != __stop_event_listeners;
 	     el++) {
-
 		__ASSERT_NO_MSG(el != NULL);
 		printk("|\t[L:%s]\n", el->name);
 	}
@@ -183,8 +176,6 @@ static void event_manager_show_subscribers(void)
 	printk("\n");
 }
 
-#ifdef CONFIG_DESKTOP_EVENT_MANAGER_PROFILER_ENABLED
-#if CONFIG_DESKTOP_EVENT_MANAGER_TRACE_EVENT_EXECUTION
 static void register_predefined_events()
 {
 	u16_t profiler_event_id;
@@ -196,15 +187,13 @@ static void register_predefined_events()
 	profiler_event_id = profiler_register_event_type("event_processing_end", NULL, NULL, 0);
 	profiler_event_ids[__stop_event_types - __start_event_types + 2] = profiler_event_id;
 }
-#endif
 
 static void register_events()
 {
 	for (const struct event_type *et = __start_event_types;
 	     (et != NULL) && (et != __stop_event_types);
 	     et++) {
-		if (et->log_info)
-		{
+		if (et->log_info) {
 			u16_t profiler_event_id;
 			profiler_event_id = profiler_register_event_type(et->name, 
 				et->log_info->log_args_labels, et->log_info->log_args_types, 
@@ -212,19 +201,22 @@ static void register_events()
 			profiler_event_ids[et - __start_event_types] = profiler_event_id;
 		}
  	}
-#if CONFIG_DESKTOP_EVENT_MANAGER_TRACE_EVENT_EXECUTION
-	register_predefined_events();
-#endif
+	if (IS_ENABLED(CONFIG_DESKTOP_EVENT_MANAGER_TRACE_EVENT_EXECUTION)) {
+		register_predefined_events();
+	}
 }
-#endif
 
 int event_manager_init(void)
 {
-#ifdef CONFIG_DESKTOP_EVENT_MANAGER_PROFILER_ENABLED
-	profiler_init();
-	register_events();
-#endif
-
+	profiler_event_ids = k_malloc((__stop_event_types - __start_event_types + 2) * sizeof(u16_t));
+	if (!profiler_event_ids) {
+		printk("event_manager: memory allocation failed \n");
+		return -1;
+	}
+	if (IS_ENABLED(CONFIG_DESKTOP_EVENT_MANAGER_PROFILER_ENABLED)) {	
+		profiler_init();
+		register_events();
+	}
 	if (IS_ENABLED(CONFIG_DESKTOP_EVENT_MANAGER_SHOW_LISTENERS)) {
 		event_manager_show_listeners();
 		event_manager_show_subscribers();
